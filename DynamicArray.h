4149595a993d2949497b9ace2c2e62024002e6e1
Стас Ладыгин <ladygin_sa_1017@1511.ru>
2020-04-25 21:07:12 +0300

    First version DynamicArray

--- DynamicArray.h
+++ DynamicArray.h
@@ -8,7 +8,6 @@ template<typename T>
 class DynamicArray
 {
 public:
-	//DynamicArray();
 	DynamicArray(size_t size = 0);
 	DynamicArray(size_t size, const T& default_element);
 	DynamicArray(const T* const arr, size_t count, size_t start = 0);
@@ -18,8 +17,8 @@ public:
 	void Resize(size_t new_size);
 	size_t Size() const;
 	DynamicArray<T>& operator=(const DynamicArray<T>& arr);
-	std::optional<T>& operator[](size_t index);
-	const std::optional<T>& operator[](size_t index) const;
+	std::optional<T>& operator[](const size_t index);
+	const std::optional<T>& operator[](const size_t index) const;
 	~DynamicArray() { delete[] this->data; }
 private:
 	std::optional<T>* data;
@@ -46,9 +45,7 @@ template<typename T>
 DynamicArray<T>::DynamicArray(size_t size, const T& default_element) : DynamicArray(size)
 {
 	for (size_t i = 0; i < this->count; i++)
-	{
 		this->data[i] = default_element;
-	}
 }
 
 template<typename T>
@@ -86,9 +83,31 @@ void DynamicArray<T>::Resize(size_t new_size)
 {
 	if (new_size > this->capacity)
 	{
+		this->capacity = new_size + DEFAULT_BUFFER_SIZE;
+		std::optional<T>* new_array;
+		//Выделение памяти под новый массив
+		try
+		{
+			new_array = new std::optional<T>[this->capacity];
+		}
+		catch (const std::bad_alloc & exc)
+		{
+			throw exc.what();
+		}
+		//Заполнение нового массива значениями из старого
+		for (size_t i = 0; i < this->count; i++)
+			new_array[i] = this->data[i];
+		//Удаление старого массива
 		delete[] this->data;
-
+		//Присвоение нового массива старому
+		this->data = new_array;
 	}
+	else
+	{
+		for (size_t i = new_size; i < this->count; i++)
+			this->data[i] = std::nullopt;
+	}
+	this->count = new_size;
 }
 
 template<typename T>
@@ -118,22 +137,26 @@ DynamicArray<T>& DynamicArray<T>::operator=(const DynamicArray<T>& arr)
 	//Для более оптимальной работы с памятью, используя capacity
 	for (size_t i = 0; i < arr.count; i++)
 		this->data[i] = arr.data[i];
-	if(this->count > arr.count)
-		for(size_t i=0; i<(this->count-arr.count); i++)
+	if (this->count > arr.count)
+		for (size_t i = arr.count; i < this->count; i++)
 			this->data[i] = std::nullopt;
 	this->count = arr.count;
 	return *this;
 }
 
 template<typename T>
-std::optional<T>& DynamicArray<T>::operator[](size_t index)
+std::optional<T>& DynamicArray<T>::operator[](const size_t index)
 {
-	return index < this->count ? this->data[index] : std::nullopt;
+	if(index >= this->count)
+		throw "Invalid index";
+	return this->data[index];
 }
 
 template<typename T>
-const std::optional<T>& DynamicArray<T>::operator[](size_t index) const
+const std::optional<T>& DynamicArray<T>::operator[](const size_t index) const
 {
-	return index < this->count ? this->data[index] : std::nullopt;
+	if (index >= this->count)
+		throw "Invalid index";
+	return this->data[index];
 }
 
--- Lab2.cpp
+++ Lab2.cpp
@@ -1,28 +1,51 @@
 #include "Lab2.h"
 #include "DynamicArray.h"
+#include "ErrorMessage.h"
 #include <string>
 #include <vector>
 #include <optional>
+#include <iostream>
 
 Lab2::Lab2(QWidget *parent)
 	: QWidget(parent)
 {
 	ui.setupUi(this);
+	std::string str;
 	int* arr = new int[10]{0,1,2,3,4,5,6,7,8,9};
-	std::vector<int> arr2;
+	struct Point
+	{
+		float x;
+		float y;
+	};
+	std::vector<Point> arr2(10);
+	Point p{};
+	for (size_t i = 0; i < arr2.size(); i++)
+	{
+		//str+=std::to_string(arr2[i].x);
+	}
 	//vec->push_back(10);
 	DynamicArray<int> *dyn = new DynamicArray<int>();
 	delete dyn;
 	DynamicArray<int> dm(arr, 4, 4);
 	DynamicArray<int> dm2(2);
-	DynamicArray<int> dm3(5);
-	dm3=dm2=dm3=dm;
-	//dm3.Set(2, 3);
-	dm3[2] = std::optional<int>{ 3 };
-	std::string str;
+	DynamicArray<int> dm3(10, 1);
+	const DynamicArray<int> dm4(10, 5);
+    dm3=dm2=dm3=dm;
+	dm3.Set(2, 3);
+	std::optional<int> op = 3;
+	dm3.operator[](3) = op;
+	try 
+	{
+		dm3[2] = dm4[11];
+	}
+	catch(const char* str)
+	{
+		OutputError(str);
+	}
 	for (int i = 0; i < dm3.Size(); i++)
 	{
-		str += std::to_string(*dm3.Get(i));
+		if(dm3[i])
+			str += std::to_string(dm3[i].value());
 	}
 	ui.lineEdit->setText(str.c_str());
 }
